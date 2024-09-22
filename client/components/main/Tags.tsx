13a00abbc67f80611a6cd8160feb7234840734d3
achoo-o <amyj.etana@gmail.com>
2024-09-22 21:16:49 +1200

    update tags

--- client/components/main/Tags.tsx
+++ client/components/main/Tags.tsx
@@ -1,29 +1,32 @@
 export default function Tags() {
   return (
-    <ul className="flex lg:flex-2 flex-row flex-wrap gap-5 justify-center items-start pt-8 lg:justify-end lg:basis-1/3">
-      <li className="py-2 px-3 bg-gray-200 rounded-lg">
+    <ul className="flex lg:flex-2 flex-row flex-wrap gap-3 justify-center items-start pt-8 lg:justify-end lg:basis-1/3 text-gray-900">
+      <li className="py-2 px-4 bg-gray-200 rounded-lg hover:bg-gray-300 transition-all">
         React
       </li>
-      <li className="py-2 px-3 bg-gray-200 rounded-lg">
-        Node
+      <li className="py-2 px-4 bg-gray-200 rounded-lg hover:bg-gray-300 transition-all">
+        Typescript
       </li>
-      <li className="py-2 px-3 bg-gray-200 rounded-lg">
+      <li className="py-2 px-4 bg-gray-200 rounded-lg hover:bg-gray-300 transition-all">
+        Node.js
+      </li>
+      <li className="py-2 px-4 bg-gray-200 rounded-lg hover:bg-gray-300 transition-all">
+        Tailwind
+      </li>
+      <li className="py-2 px-4 bg-gray-200 rounded-lg hover:bg-gray-300 transition-all">
+        Git
+      </li>
+      <li className="py-2 px-4 bg-gray-200 rounded-lg hover:bg-gray-300 transition-all">
+        Github
+      </li>
+      <li className="py-2 px-4 bg-gray-200 rounded-lg hover:bg-gray-300 transition-all">
         Knex
       </li>
-      <li className="py-2 px-3 bg-gray-200 rounded-lg">
+      <li className="py-2 px-4 bg-gray-200 rounded-lg hover:bg-gray-300 transition-all">
         Sass
       </li>
-      <li className="py-2 px-3 bg-gray-200 rounded-lg">
-        React
-      </li>
-      <li className="py-2 px-3 bg-gray-200 rounded-lg">
-        Node
-      </li>
-      <li className="py-2 px-3 bg-gray-200 rounded-lg">
-        Knex
-      </li>
-      <li className="py-2 px-3 bg-gray-200 rounded-lg">
-        Sass
+      <li className="py-2 px-4 bg-gray-200 rounded-lg hover:bg-gray-300 transition-all">
+        Vitest
       </li>
     </ul>
   )
